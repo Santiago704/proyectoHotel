a0acb7421e2f547e49290d1773c3905459c3b58e
santiago <smunera704@gmail.com>
2025-04-27 18:42:13 -0500

    cambios para guardar usuario en base de datos

---
+++ application/main.py
@@ -0,0 +1,11 @@
+from view.menu import menu_app
+
+
+def print_hi():
+    app = menu_app()
+    app.init_app()
+
+print_hi()
+
+
+
--- repository/conexion/bd_connection.py
+++ repository/conexion/bd_connection.py
@@ -1,47 +1,47 @@
-import mysql
-
-from mysql import connector
-
+import mysql.connector  # Corregí el import, es mejor usar directamente `mysql.connector`
 
 class Conexion:
 
-
-    def __init__(self, host, port, user, password , database):
+    def __init__(self, host, port, user, password, database):
         self.host = host
         self.port = port
         self.user = user
         self.password = password
         self.database = database
+        self._connection = None
 
     def connection(self):
         try:
-            self.connection = mysql.connector.connect(
-                host = self.host,
-                port = self.port,
-                user = self.user,
-                password = self.password,
-                database = self.database
+            self._connection = mysql.connector.connect(
+                host=self.host,
+                port=self.port,
+                user=self.user,
+                password=self.password,
+                database=self.database
             )
-            print("Conexión Establecidad")
+            print("Conexión Establecida")
         except mysql.connector.Error as err:
-            print("error al conectar a la base de datos ", err)
+            print("Error al conectar a la base de datos:", err)
 
     def disconnect(self):
-        if self.connection:
-            self.connection.close()
+        if self._connection:
+            self._connection.close()
             print("Conexión Cerrada")
 
-    def execute_query(self, query , params= None):
-        cursor = self.connection.cursor(buffered= True)
+    def execute_query(self, query, params=None):
+        if not self._connection:
+            print("No hay conexión establecida.")
+            return None
+        cursor = self._connection.cursor(buffered=True)
         try:
             cursor.execute(query, params)
-            self.connection.commit()
-            print("Registro se guardo exitosamente")
+            self._connection.commit()
+            print("Registro guardado exitosamente")
             if query.lower().startswith('select'):
-                result =cursor.fetchall()
+                result = cursor.fetchall()
                 return result
         except mysql.connector.Error as err:
-            print("Error al ejecutar la consulta", err)
+            print("Error al ejecutar la consulta:", err)
             return None
         finally:
             cursor.close()
